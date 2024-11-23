bfeb424fd3fe26816e8bee4fa23dee9a444d4066
Lakshika jayaweera <lakshikakalpani41@gmail.com>
2024-11-23 19:23:08 +0530

    first commit

---
+++ anim.js
@@ -0,0 +1,14 @@
+
+var audio = document.querySelector("audio");
+
+
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; 
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); 
+}
+
+setTimeout(ocultarTitulo, 216000);
